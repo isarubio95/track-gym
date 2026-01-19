20455eb2e9416fc0a7eb95bebd67dd7f82a2ff97
isarubio95 <isarubio95@gmail.com>
2026-01-19 11:37:00 +0100

    implementando login

--- djangoproject/exampleapp/views.py
+++ djangoproject/exampleapp/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+@login_required
+def homeView(request):
+    """View function for the home page of ExampleApp."""
+    return render(request, "index.html")
