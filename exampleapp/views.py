37daf4ff8a34205ba05969fbf962683a1cea2abf
isarubio95 <isarubio95@gmail.com>
2026-02-04 18:30:53 +0100

    eliminación de import no usado

--- exampleapp/views.py
+++ exampleapp/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import ActividadForm, DaisySignupForm
